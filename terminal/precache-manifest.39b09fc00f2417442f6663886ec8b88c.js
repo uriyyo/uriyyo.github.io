ec0ec343d00d956a43288b2c4296c3f8b394d306
uriyyo <1998uriyyo@gmail.com>
2020-05-15 17:43:53 +0300

    :tada: Add latest page build

---
+++ terminal/precache-manifest.39b09fc00f2417442f6663886ec8b88c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2231d113cce5a6255e8563611866ef9c",
+    "url": "/terminal/index.html"
+  },
+  {
+    "revision": "dd5ed5339d15ea1ed91c",
+    "url": "/terminal/static/css/main.8714c783.chunk.css"
+  },
+  {
+    "revision": "ae1e195d555d6516e9b3",
+    "url": "/terminal/static/js/2.ca5f12ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/terminal/static/js/2.ca5f12ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5ed5339d15ea1ed91c",
+    "url": "/terminal/static/js/main.677cef75.chunk.js"
+  },
+  {
+    "revision": "73abde12f7b546383512",
+    "url": "/terminal/static/js/runtime-main.f01c2400.js"
+  }
+]);
